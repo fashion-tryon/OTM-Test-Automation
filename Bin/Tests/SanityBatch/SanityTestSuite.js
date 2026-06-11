36c5737b9409cdaf22996f292bf7e1183b7daca7
VVIS Tech <admin@vvistech.com>
2026-06-11 10:50:52 +0530

    Remove node_modules, Bin, credentials from tracking

--- Bin/Tests/SanityBatch/SanityTestSuite.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-require('./Test_01_Login.js');
-require('./Test_18_NavigateToOrderReleaseFinder.js');
-require('./Test_19_NavigateToShipmentFinder.js');
-require('./Test_20_OBIEEConsoleLogin.js');
-require('./Test_21_BIPConsoleLogin.js');
